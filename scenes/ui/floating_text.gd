d5253d299647784b894ce60d5b9320d37a46e4ea
Justin Siegal <justin.siegal@gmail.com>
2024-08-18 22:07:14 -0600

    adding damage flash (lesson 45)

--- scenes/ui/floating_text.gd
+++ scenes/ui/floating_text.gd
@@ -10,12 +10,14 @@ func start(text: String):
 	var tween = create_tween()
 	tween.set_parallel()
 	
+	#move the damage text up 16 pixels over .3 secs
 	tween.tween_property(self, "global_position", global_position + (Vector2.UP) * 16, .3)\
 		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	
 	
 	tween.chain()
 	
+	#move the damage text up an additional 32 pixels and scale down to 0 over .5 secs
 	tween.tween_property(self, "global_position", global_position + (Vector2.UP) * 48, .5)\
 		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property(self, "scale", Vector2.ZERO, .5)\
@@ -25,6 +27,7 @@ func start(text: String):
 	
 	tween.tween_callback(queue_free)
 	
+	#sclae the damage text to 1.5 over .15s
 	var scale_tween = create_tween()
 	scale_tween.tween_property(self, "scale", Vector2.ONE * 1.5, .15)\
 		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
