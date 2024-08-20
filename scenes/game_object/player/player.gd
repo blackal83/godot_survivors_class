39d7176d97ddda318411f8cc4ec29fdf40e6117d
Justin Siegal <justin.siegal@gmail.com>
2024-08-19 22:12:52 -0600

    adding vial and player hit sfx (lesson 57)

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -64,6 +64,7 @@ func on_damage_interval_timer_timeout():
 	check_deal_damage()
 
 func on_health_changed():
+	$HitRandomStreamPlayerComponent.play_random()
 	GameEvents.emit_player_damaged()
 	update_health_display()
 
